af73ba1f02d6a7eae093c39b47b0f4e657d983d3
Vishal Talwar <vtalwar.w@gmail.com>
2022-10-07 14:32:26 -0700

    adds failing test

--- src/others/add.test.ts
+++ src/others/add.test.ts
@@ -2,7 +2,7 @@ import { add } from './add'
 
 describe('test add function', () => {
   it('should return 15 for add(10,5)', () => {
-    expect(add(10, 5)).toBe(15)
+    expect(add(10, 5)).toBe(16)
   })
 
   it('should return 5 for add(2,3)', () => {
